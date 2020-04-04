1d0a1cca6ac8c5f4a3cf796a66756e1a8c068141
Ruslan Salkaev <rsalkaev@gmail.com>
2020-04-04 15:37:06 +0300

    Complete Task2

--- hash-sum-plugin/src/main/kotlin/org/jetbrains/internship/HashSumPlugin.kt
+++ hash-sum-plugin/src/main/kotlin/org/jetbrains/internship/HashSumPlugin.kt
@@ -12,35 +12,83 @@ class HashSumPlugin : Plugin<Project> {
         p0.tasks.create("calculateSha1") {
             doFirst {
                 val dir = p0.rootDir.absolutePath
-                val fileName = "hash.txt"
-                val extensionsArray = arrayListOf("kt", "java")
-                //Будем хранить stack digest-ов подпроектов в которых мы находимся
-                val stack = Stack<Pair<MessageDigest, String>>()
-                File(dir).walkTopDown().forEach { currentPath ->
-                    //если мы вышли из подроекта, то записываем в его build директорию файл с нашим хешем
-                    while (!stack.empty() && !currentPath.absolutePath.startsWith(stack.peek().second)) {
-                        val file = File("${stack.peek().second}\\build\\$fileName")
-                        file.writeText(stack.peek().first.digest().toHexString())
-                        stack.pop()
-                    }
-                    // Проверяем зашли ли мы в очередной подпроект
-                    if (File("${currentPath.absolutePath}\\build").exists()) {
-                        stack.push(Pair(MessageDigest.getInstance("SHA-1"), currentPath.absolutePath))
-                    }
-                    if (extensionsArray.contains(currentPath.extension)) {
-                        val textBytes = currentPath.readBytes()
-                        stack.forEach {
-                            it.first.update(textBytes, 0, textBytes.size)
-                        }
-                    }
-                }
-                //Обрабатываем последний подпроект
-                while (!stack.empty()) {
-                    val file = File("${stack.peek().second}\\build\\$fileName")
-                    file.writeText(stack.peek().first.digest().toHexString())
-                    stack.pop()
+                val hashType = "SHA-1"
+                countHash(dir, hashType)
+            }
+        }
+        p0.tasks.create("calculateMd2") {
+            doFirst {
+                val dir = p0.rootDir.absolutePath
+                val hashType = "MD2"
+                countHash(dir, hashType)
+            }
+        }
+        p0.tasks.create("calculateMd5") {
+            doFirst {
+                val dir = p0.rootDir.absolutePath
+                val hashType = "MD5"
+                countHash(dir, hashType)
+            }
+        }
+        p0.tasks.create("calculateSha224") {
+            doFirst {
+                val dir = p0.rootDir.absolutePath
+                val hashType = "SHA-224"
+                countHash(dir, hashType)
+            }
+        }
+        p0.tasks.create("calculateSha256") {
+            doFirst {
+                val dir = p0.rootDir.absolutePath
+                val hashType = "SHA-256"
+                countHash(dir, hashType)
+            }
+        }
+        p0.tasks.create("calculateSha384") {
+            doFirst {
+                val dir = p0.rootDir.absolutePath
+                val hashType = "SHA-384"
+                countHash(dir, hashType)
+            }
+        }
+        p0.tasks.create("calculateSha512") {
+            doFirst {
+                val dir = p0.rootDir.absolutePath
+                val hashType = "SHA-512"
+                countHash(dir, hashType)
+            }
+        }
+    }
+
+    private fun countHash(dir: String, hashType: String) {
+        val fileName = "hash.txt"
+        val extensionsArray = arrayListOf("kt", "java")
+        //Будем хранить stack digest-ов подпроектов в которых мы находимся
+        val stack = Stack<Pair<MessageDigest, String>>()
+        File(dir).walkTopDown().forEach { currentPath ->
+            //если мы вышли из подроекта, то записываем в его build директорию файл с нашим хешем
+            while (!stack.empty() && !currentPath.absolutePath.startsWith(stack.peek().second)) {
+                val file = File("${stack.peek().second}\\build\\$fileName")
+                file.writeText(stack.peek().first.digest().toHexString())
+                stack.pop()
+            }
+            // Проверяем зашли ли мы в очередной подпроект
+            if (File("${currentPath.absolutePath}\\build").exists()) {
+                stack.push(Pair(MessageDigest.getInstance(hashType), currentPath.absolutePath))
+            }
+            //доавляем очередной файл с подходящим расширением в хеш всех подпроектов, в которых сейчас находимся
+            if (extensionsArray.contains(currentPath.extension)) {
+                val textBytes = currentPath.readBytes()
+                stack.forEach {
+                    it.first.update(textBytes, 0, textBytes.size)
                 }
             }
+        }
+        //обрабатываем подпроекты, в которох мы были на момент конца
+        while (!stack.empty()) {
+            val file = File("${stack.peek().second}\\build\\$fileName")
+            file.writeText(stack.peek().first.digest().toHexString())
+            stack.pop()
         }
     }
 
--- hash-sum-plugin/src/test/kotlin/org/jetbrains/internship/HashSumPluginTest.kt
+++ hash-sum-plugin/src/test/kotlin/org/jetbrains/internship/HashSumPluginTest.kt
@@ -10,12 +10,36 @@ import java.io.File
 
 class HashSumPluginTest {
     companion object {
-        val SHA_1_FILE_PATH: String
+        val FILE_PATH: String
             get() = "hash.txt"
         const val CALCULATE_SHA_1_TASK_NAME = "calculateSha1"
+        const val CALCULATE_MD_2_TASK_NAME = "calculateMd2"
+        const val CALCULATE_MD_5_TASK_NAME = "calculateMd5"
+        const val CALCULATE_SHA_224_TASK_NAME = "calculateSha224"
+        const val CALCULATE_SHA_256_TASK_NAME = "calculateSha256"
+        const val CALCULATE_SHA_384_TASK_NAME = "calculateSha384"
+        const val CALCULATE_SHA_512_TASK_NAME = "calculateSha512"
 
         val String.isSHA1Content: Boolean
             get() = matches("^[a-fA-F0-9]{40}$".toRegex())
+
+        val String.isMD2Content: Boolean
+            get() = matches("^[a-fA-F0-9]{32}$".toRegex())
+
+        val String.isMD5Content: Boolean
+            get() = matches("^[a-fA-F0-9]{32}$".toRegex())
+
+        val String.isSHA224Content: Boolean
+            get() = matches("^[a-fA-F0-9]{56}$".toRegex())
+
+        val String.isSHA256Content: Boolean
+            get() = matches("^[a-fA-F0-9]{64}$".toRegex())
+
+        val String.isSHA384Content: Boolean
+            get() = matches("^[a-fA-F0-9]{96}$".toRegex())
+
+        val String.isSHA512Content: Boolean
+            get() = matches("^[a-fA-F0-9]{128}$".toRegex())
 
     }
 
@@ -37,15 +61,80 @@ class HashSumPluginTest {
     }
 
     @Test
-    fun task1Test() {
+    fun taskSha1Test() {
         val calculateSha1Task = project.tasks.findByPath(CALCULATE_SHA_1_TASK_NAME)
         assertNotNull(calculateSha1Task)
         calculateSha1Task!!.actions.forEach {
             it.execute(calculateSha1Task)
         }
-        val hashSumFile = project.buildDir.resolve(SHA_1_FILE_PATH)
+        val hashSumFile = project.buildDir.resolve(FILE_PATH)
         assertTrue(hashSumFile.exists() && hashSumFile.readText().isSHA1Content)
     }
 
+    @Test
+    fun taskMd2Test() {
+        val calculateMd2Task = project.tasks.findByPath(CALCULATE_MD_2_TASK_NAME)
+        assertNotNull(calculateMd2Task)
+        calculateMd2Task!!.actions.forEach {
+            it.execute(calculateMd2Task)
+        }
+        val hashSumFile = project.buildDir.resolve(FILE_PATH)
+        assertTrue(hashSumFile.exists() && hashSumFile.readText().isMD2Content)
+    }
+
+    @Test
+    fun taskMd5Test() {
+        val calculateMd5Task = project.tasks.findByPath(CALCULATE_MD_5_TASK_NAME)
+        assertNotNull(calculateMd5Task)
+        calculateMd5Task!!.actions.forEach {
+            it.execute(calculateMd5Task)
+        }
+        val hashSumFile = project.buildDir.resolve(FILE_PATH)
+        assertTrue(hashSumFile.exists() && hashSumFile.readText().isMD5Content)
+    }
+
+    @Test
+    fun taskSha224Test() {
+        val calculateSha224Task = project.tasks.findByPath(CALCULATE_SHA_224_TASK_NAME)
+        assertNotNull(calculateSha224Task)
+        calculateSha224Task!!.actions.forEach {
+            it.execute(calculateSha224Task)
+        }
+        val hashSumFile = project.buildDir.resolve(FILE_PATH)
+        assertTrue(hashSumFile.exists() && hashSumFile.readText().isSHA224Content)
+    }
+
+    @Test
+    fun taskSha256Test() {
+        val calculateSha256Task = project.tasks.findByPath(CALCULATE_SHA_256_TASK_NAME)
+        assertNotNull(calculateSha256Task)
+        calculateSha256Task!!.actions.forEach {
+            it.execute(calculateSha256Task)
+        }
+        val hashSumFile = project.buildDir.resolve(FILE_PATH)
+        assertTrue(hashSumFile.exists() && hashSumFile.readText().isSHA256Content)
+    }
+
+    @Test
+    fun taskSha384Test() {
+        val calculateSha384Task = project.tasks.findByPath(CALCULATE_SHA_384_TASK_NAME)
+        assertNotNull(calculateSha384Task)
+        calculateSha384Task!!.actions.forEach {
+            it.execute(calculateSha384Task)
+        }
+        val hashSumFile = project.buildDir.resolve(FILE_PATH)
+        assertTrue(hashSumFile.exists() && hashSumFile.readText().isSHA384Content)
+    }
+
+    @Test
+    fun taskSha512Test() {
+        val calculateSha512Task = project.tasks.findByPath(CALCULATE_SHA_512_TASK_NAME)
+        assertNotNull(calculateSha512Task)
+        calculateSha512Task!!.actions.forEach {
+            it.execute(calculateSha512Task)
+        }
+        val hashSumFile = project.buildDir.resolve(FILE_PATH)
+        assertTrue(hashSumFile.exists() && hashSumFile.readText().isSHA512Content)
+    }
 }
 
